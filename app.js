9f7707990d286700938cf84a718accaa4bad1415
icony/精武陈真 <chen_zhen@hisuntech.com>
2019-10-20 23:34:39 +0800

    onError onLinstening

--- app.js
+++ app.js
@@ -16,6 +16,40 @@ app.use("/**", function (req, res) {
 // create static server for express 
 const server = http.createServer(app);
 server.listen(port);
+server.on("error",onError)
+server.on('listening', onListening);
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+      console.log('Listening on ' + bind); 
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
 
 console.log(`env:${process.env.env}`);
 console.log(`path:${staticPath}`);
